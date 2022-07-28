d0608446df699f62117662385c95bcaae0c45659
Osama Shaikh <shaikhosama504@gmail.com>
2022-07-28 15:12:38 +0530

    environment.prod fix

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -3,5 +3,4 @@ export const environment = {
   client_id: 'aacbc30e7608457fa554bf7b9bcfc9ab',
   client_secret: '108bc45f53b845d7b52a1c178534015a',
   server_url: 'https://staging-chat-app.herokuapp.com',
-
 };
